71d33897b7de15b60626a31cff6e24a6d18079d0
crkaide <crkaide@ilstu.edu>
2021-05-19 21:20:49 -0500

    module code added static/js/app.js

--- static/js/app.js
+++ static/js/app.js
@@ -59,25 +59,46 @@
 // -----------------------------------------------------------------------------------------------------
 
 
+// // BELOW BUILT DURING MODULE
+// // import the data from data.js 
+// const tableData = data;
 
-// import the data from data.js 
-const tableData = data;
+// // Reference the HTML table using d3 
+// var tbody = d3.select("tbody");
 
-// Reference the HTML table using d3 
-var tbody = d3.select("tbody");
+// // Create new function
+// function buildTable(data) {
+//     tbody.html("");
+//     data.forEach((dataRow) => {
+//         let row = tbody.append("tr");
+//         Object.values(dataRow).forEach((val) => {
+//             let cell = row.append("td");
+//             cell.text(val);
+//             }
+//         );
+//     });
+// }
 
-// Create new function
+// Module version with comments:
+
 function buildTable(data) {
+    // First, clear out any existing data
     tbody.html("");
+  
+    // Next, loop through each object in the data
+    // and append a row and cells for each value in the row
     data.forEach((dataRow) => {
-        let row = tbody.append("tr");
-        Object.values(dataRow).forEach((val) => {
-            let cell = row.append("td");
-            cell.text(val);
-            }
-        );
-        });
-}
-
-
+      // Append a row to the table body
+      let row = tbody.append("tr");
+  
+      // Loop through each field in the dataRow and add
+      // each value as a table cell (td)
+      Object.values(dataRow).forEach((val) => {
+        let cell = row.append("td");
+        cell.text(val);
+        }
+      );
+    });
+  }
+  
 
