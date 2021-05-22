5fdee2a64b2b1bf04789a2e4da7a015a7d407aad
crkaide <crkaide@ilstu.edu>
2021-05-22 18:09:11 -0500

    app changes

--- static/js/app.js
+++ static/js/app.js
@@ -56,16 +56,19 @@ function updateFilters() {
   
   // 7. Use this function to filter the table when data is entered.
   function filterTable() {
-  
+
     // 8. Set the filtered data to the tableData.
-    
+    let filteredData = tableData;
   
     // 9. Loop through all of the filters and keep any data that
     // matches the filter values
+    Object.entries(filters).forEach(([filterId, elementValue]) => {
+      filteredData = filteredData.filter(row => row[filterId] === elementValue);
+    });
     
-  
     // 10. Finally, rebuild the table using the filtered data
-    
+    buildTable(filtered_data);
+  
   }
   
   // 2. Attach an event to listen for changes to each filter
